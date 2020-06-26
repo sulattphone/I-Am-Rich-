b9ba1b3b09c989dff27b804bde753629e81c4a2e
Su Latt Phone <sulattphone@gmail.com>
2020-06-25 21:31:43 -0700

    Add files via upload

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Su Latt Phone on 6/12/20.
+//  Copyright © 2020 sulattphone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
